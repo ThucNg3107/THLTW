8c4c824234fcc93b3dc0a8745c460329bcec9744
thucnguyenhien102 <thucnguyenhien102@gmail.com>
2025-03-18 11:21:28 +0700

    up

---
+++ 2280603165_NguyenHienThuc_Week3/2280603165_NguyenHienThuc_Week3/Migrations/20250317222056_SeedCategories.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace _2280603165_NguyenHienThuc_Week3.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Điện tử" },
+                    { 2, "Gia dụng" },
+                    { 3, "Thời trang" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
---
+++ 2280603165_NguyenHienThuc_Week3/2280603165_NguyenHienThuc_Week3/Models/SD.cs
@@ -0,0 +1,10 @@
+﻿namespace _2280603165_NguyenHienThuc_Week3.Models
+{
+    public class SD
+    {
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+    }
+}
