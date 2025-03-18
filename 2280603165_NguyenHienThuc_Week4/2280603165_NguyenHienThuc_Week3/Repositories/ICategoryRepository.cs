957d191e5c23f94c70cb9095e98f260c7bc3ed0f
thucnguyenhien102 <thucnguyenhien102@gmail.com>
2025-03-18 18:56:43 +0700

    up

---
+++ 2280603165_NguyenHienThuc_Week4/2280603165_NguyenHienThuc_Week3/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using _2280603165_NguyenHienThuc_Week3.Models;
+
+namespace _2280603165_NguyenHienThuc_Week3.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category> GetByIdAsync(int id);
+        Task AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(int id);
+    }
+}
