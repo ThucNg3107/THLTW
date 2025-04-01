72405fd1da5c88e51ff36c75739559dae9523236
thucnguyenhien102 <thucnguyenhien102@gmail.com>
2025-04-01 10:04:29 +0700

    up

---
+++ 2280603165_NguyenHienThuc_Week5/2280603165_NguyenHienThuc_Week3/Models/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using _2280603165_NguyenHienThuc_Week3.Models;
+using Microsoft.EntityFrameworkCore;
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<ProductImage> ProductImages { get; set; }
+    public DbSet<Orders> Orders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Seed dữ liệu mẫu cho Category
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Điện tử" },
+            new Category { Id = 2, Name = "Gia dụng" },
+            new Category { Id = 3, Name = "Thời trang" }
+        );
+    }
+}
